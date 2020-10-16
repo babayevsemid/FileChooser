95ed50452e1f7f75b2f60fb4811b56fec8e5d3e1
semidb <babayevsemid@gmail.com>
2020-10-16 11:23:29 +0400

    file model

--- library/src/main/java/com/semid/library/enums/ChooseTypeEnum.java
+++ library/src/main/java/com/semid/library/enums/ChooseTypeEnum.java
@@ -3,7 +3,7 @@ package com.semid.library.enums;
 public enum ChooseTypeEnum {
     TAKE_VIDEO(3232), CHOOSE_VIDEO(3233), TAKE_PHOTO(3234), CHOOSE_PHOTO(3235), CHOOSE_FILE(3236);
 
-    int id;
+    public int id;
 
     ChooseTypeEnum(int id) {
         this.id = id;
