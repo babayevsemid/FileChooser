4527f037aa6bdae02a894afcdffd49b40ef88ee2
Samid Babayev <developer_i5@Samid-iMac.local>
2021-08-18 12:18:26 +0400

    Update libraries

--- FileChooser/src/main/java/com/semid/filechooser/SingleLiveEvent.kt
+++ FileChooser/src/main/java/com/semid/filechooser/SingleLiveEvent.kt
@@ -1,62 +1,34 @@
 package com.semid.filechooser
 
 import androidx.annotation.MainThread
-import androidx.lifecycle.LifecycleOwner
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.Observer
+import androidx.lifecycle.*
 import java.util.concurrent.CopyOnWriteArraySet
 import java.util.concurrent.atomic.AtomicBoolean
 
 class SingleLiveEvent<T> : MutableLiveData<T>() {
+    private lateinit var liveDataToObserve: LiveData<T>
+    private val mPending = AtomicBoolean(false)
 
-    private val observers = CopyOnWriteArraySet<ObserverWrapper<in T>>()
+    init {
+        val outputLiveData = MediatorLiveData<T>()
+        outputLiveData.addSource(this) { currentValue: T ->
+            outputLiveData.value = currentValue
+            mPending.set(false)
+        }
+        liveDataToObserve = outputLiveData
+    }
 
     @MainThread
     override fun observe(owner: LifecycleOwner, observer: Observer<in T>) {
-        val wrapper = ObserverWrapper(observer)
-        observers.add(wrapper)
-        super.observe(owner, wrapper)
-    }
-
-    override fun removeObservers(owner: LifecycleOwner) {
-        observers.clear()
-        super.removeObservers(owner)
-    }
-
-    override fun removeObserver(observer: Observer<in T>) {
-        observers.remove(observer)
-        super.removeObserver(observer)
-    }
-
-    @MainThread
-    override fun setValue(t: T?) {
-        observers.forEach { it.newValue() }
-        super.setValue(t)
-    }
-
-    /**
-     * Used for cases where T is Void, to make calls cleaner.
-     */
-
-    /*
-    @MainThread
-    fun call() {
-        value = null
-    }
-     */
-
-    private class ObserverWrapper<T>(private val observer: Observer<T>) : Observer<T> {
-
-        private val pending = AtomicBoolean(false)
-
-        override fun onChanged(t: T?) {
-            if (pending.compareAndSet(true, false)) {
+        liveDataToObserve.observe(owner, { t: T ->
+            if (mPending.get())
                 observer.onChanged(t)
-            }
-        }
+        })
+    }
 
-        fun newValue() {
-            pending.set(true)
-        }
+    @MainThread
+    override fun setValue(value: T?) {
+        mPending.set(true)
+        super.setValue(value)
     }
 }
