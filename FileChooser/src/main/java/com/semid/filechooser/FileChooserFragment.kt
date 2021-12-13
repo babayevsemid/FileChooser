0134e6542804a9b9e9d68a42cc6064ba3a368313
Samid Babayev <samid@Samids-MacBook-Pro.local>
2021-12-13 21:45:47 +0400

    Android 11

--- FileChooser/src/main/java/com/semid/filechooser/FileChooserFragment.kt
+++ FileChooser/src/main/java/com/semid/filechooser/FileChooserFragment.kt
@@ -1,28 +1,18 @@
 package com.semid.filechooser
 
 import android.Manifest
-import android.R.attr
 import android.app.Activity.RESULT_OK
-import android.content.ContentValues
 import android.content.Context
 import android.content.Intent
-import android.database.Cursor
-import android.net.Uri
 import android.os.StrictMode
 import android.provider.MediaStore
-import android.util.Log
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.FileProvider
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.LiveData
 import kotlinx.coroutines.*
 import java.io.File
-import android.os.Environment
-import android.R.attr.data
-
-import android.graphics.Bitmap
-import androidx.core.content.FileProvider
-import java.io.IOException
 
 
 class FileChooserFragment(private var fragment: Fragment) {
